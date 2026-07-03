bf8744f95319004077a23535d93a980f38b73a2f
ahc_1023 <kashn4655@gmail.com>
2026-07-03 15:51:00 +0900

    bugfix: evaluate, pick_best_action 외부 weights 입력으로 변경 (#36)

--- algorithm/evaluator.py
+++ algorithm/evaluator.py
@@ -4,17 +4,17 @@ from .feature_context import feature_context
 from .feature_spec import FEATURES
 
 # 각 feature_spec에 대해, feature_context를 받아서 점수를 계산하는 함수
-def evaluate(ctx: feature_context) -> float:
+def evaluate(ctx: feature_context, weights: dict[str, float]) -> float:
     score = 0.0
     for feature_spec in FEATURES:
         feature_func = feature_spec.func
-        weight = feature_spec.weight
+        weight = weights.get(feature_spec.name, feature_spec.weight)
         score += weight * feature_func(ctx)
     return score
 
 
 # 주어진 보드 상태에서 가능한 모든 액션 중, 가장 높은 점수를 가진 액션을 선택하는 함수
-def pick_best_action(actions, board) -> Action:
+def pick_best_action(actions, board, weights: dict[str, float]) -> Action:
     best_action = None
     best_score = float('-inf')
 
@@ -24,7 +24,7 @@ def pick_best_action(actions, board) -> Action:
         r2, c2 = action.bottom_right
         next_board[r1:r2+1, c1:c2+1] = 0
         ctx = feature_context.from_board(next_board)
-        score = evaluate(ctx)
+        score = evaluate(ctx, weights)
 
         if score > best_score:
             best_score = score
