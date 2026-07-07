e1727df53aadf814438a00f23aa7126d0f27385f
안희찬 <kashn4655@gmail.com>
2026-07-07 15:36:27 +0900

    bugfix: class name import 구문 오타 수정

--- algorithm/feature_spec.py
+++ algorithm/feature_spec.py
@@ -1,15 +1,18 @@
 from dataclasses import dataclass
 from typing import Callable
-from .feature_context import feature_context
+from .feature_context import FeatureContext
 from . import features
 
 @dataclass
 class FeatureSpec:
     name: str
-    func: Callable[[feature_context], float]
+    func: Callable[[FeatureContext], float]
     weight: float
 
 FEATURES = [
     FeatureSpec("remove_nine", features.feature_remove_nine, weight = 1.0),
+    FeatureSpec("start_at_middle", features.feature_start_at_middle, weight = 1.0),
+    FeatureSpec("start_at_side", features.feature_start_at_side, weight = 1.0),
     
 ]
+
