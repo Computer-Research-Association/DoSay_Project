89662268b1e56466ca3cdbd9715e0ed4b037f64d
김두훈 <seer32ku6@gmail.com>
2026-07-07 16:57:50 +0900

    create valid actions

--- algorithm/feature_context.py
+++ algorithm/feature_context.py
@@ -1,14 +1,23 @@
 import numpy as np
 from numpy.typing import NDArray
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from game.board import compute_prefix_sum
-
+from game.action import Action
+from game.board import Board
 
 @dataclass 
 class FeatureContext:
     board: NDArray[np.int8] # 숫자 배열의 좌푯값
     prefix: NDArray  # 누적합
     count_by_value: dict[int, int]  #숫자별 남아있는 개수
+    action : Action
+    _valid_actions: list | None = field(default=None, repr=False)
+    
+    @property
+    def valid_actions(self): #valid action 함수 불러오기
+        if self._valid_actions is None:
+            self._valid_actions = Board.get_valid_actions(self.board)  # next_board 기준으로 딱 1번
+        return self._valid_actions
 
     @classmethod 
     def from_board(cls, board: NDArray[np.int8]) -> "FeatureContext":
