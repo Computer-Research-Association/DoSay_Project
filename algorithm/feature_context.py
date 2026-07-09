acbd7fab3b85e9ec2e24404d9a9c2d33efa9e190
김두훈 <seer32ku6@gmail.com>
2026-07-09 12:45:08 +0900

    bug fix & Feature context modified

--- algorithm/feature_context.py
+++ algorithm/feature_context.py
@@ -7,7 +7,8 @@ from game.board import Board
 
 @dataclass 
 class FeatureContext:
-    board: NDArray[np.int8] # 숫자 배열의 좌푯값
+    board_array: NDArray[np.int8] # 숫자 배열의 좌푯값
+    boared_obj : Board #Board 인스턴트
     prefix: NDArray  # 누적합
     count_by_value: dict[int, int]  #숫자별 남아있는 개수
     action : Action
@@ -16,16 +17,17 @@ class FeatureContext:
     @property
     def valid_actions(self): #valid action 함수 불러오기
         if self._valid_actions is None:
-            self._valid_actions = Board.get_valid_actions(self.board)  # next_board 기준으로 딱 1번
+            self._valid_actions = self.board_obj.get_valid_actions(self.board)  # next_board 기준으로 딱 1번
         return self._valid_actions
 
     @classmethod 
-    def from_board(cls, board: NDArray[np.int8]) -> "FeatureContext":
-        prefix = compute_prefix_sum(board)
-        counts = {v: int((board == v).sum()) for v in range(1, 10)}
-        return cls(board=board, prefix = prefix, count_by_value = counts)
-    
-    
+    def from_board(cls, board_obj:Board) -> "FeatureContext":
+        board_array = board_obj.board
+        prefix = compute_prefix_sum(board_array)
+        counts = {v: int((board_array == v).sum()) for v in range(1, 10)}
+        return cls(board=board_array, prefix = prefix, count_by_value = counts)
+
+
 
     
 
--- algorithm/features.py
+++ algorithm/features.py
@@ -53,7 +53,7 @@ def feature_remove_five(ctx: FeatureContext) -> float:
 #9와 짝이 되는 valid action이 있는지 여부 확인
 def _has_nine_one_pair(ctx : FeatureContext) -> bool:
 
-    for action in ctx.valid_actions:
+    for action in ctx._valid_actions:
         r1, c1 = action.top_left
         r2, c2 = action.bottom_right
         region = ctx.board[r1:r2+1, c1:c2+1]
