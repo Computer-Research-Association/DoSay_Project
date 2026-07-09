1847fe9bcdaa22cbf1c3bd359320e07a3e018db8
김두훈 <seer32ku6@gmail.com>
2026-07-09 17:12:35 +0900

    from_board parameter modified

--- algorithm/feature_context.py
+++ algorithm/feature_context.py
@@ -24,8 +24,7 @@ class FeatureContext:
         return self._valid_actions
 
     @classmethod 
-    def from_board(cls, board_obj:Board, action) -> "FeatureContext":
-        board_array = board_obj.grid
+    def from_board(cls, board_array, action) -> "FeatureContext":
         prefix = compute_prefix_sum(board_array)
         counts = {v: int((board_array == v).sum()) for v in range(1, 10)}
         return cls(board=board_array, prefix = prefix, count_by_value = counts, action = action )
