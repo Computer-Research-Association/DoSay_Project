94c263fe3269ee140abbddd7e1d7efba0b685e87
안희찬 <kashn4655@gmail.com>
2026-07-09 13:18:29 +0900

    bugfix: from_board에 action 변수 추가

--- algorithm/feature_context.py
+++ algorithm/feature_context.py
@@ -24,14 +24,8 @@ class FeatureContext:
         return self._valid_actions
 
     @classmethod 
-    def from_board(cls, board_obj:Board) -> "FeatureContext":
+    def from_board(cls, board_obj:Board, action) -> "FeatureContext":
         board_array = board_obj.board
         prefix = compute_prefix_sum(board_array)
         counts = {v: int((board_array == v).sum()) for v in range(1, 10)}
-        return cls(board=board_array, prefix = prefix, count_by_value = counts)
-
-
-
-    
-
-    
+        return cls(board=board_array, prefix = prefix, count_by_value = counts, action = action)
