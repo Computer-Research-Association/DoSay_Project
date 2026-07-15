0c3579b5ada30746615a857be41764770fe00794
ahc_1023 <kashn4655@gmail.com>
2026-07-15 21:03:57 +0900

    bugfix: 상대경로 모두 절대경로로 변경 (#82)

--- feature_assistance/feature_spec.py
+++ feature_assistance/feature_spec.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from typing import Callable
-from .feature_context import FeatureContext
-from ..feature import features
+from feature_context import FeatureContext
+from feature import features
 
 @dataclass
 class FeatureSpec:
